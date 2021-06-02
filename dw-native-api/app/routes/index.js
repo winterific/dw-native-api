bc0c20d9d96601e89c67caea341bd558adda0e51
Sam Winter <sam.winter@gmail.com>
2021-06-01 21:35:42 -0400

    hi

--- dw-native-api/app/routes/index.js
+++ dw-native-api/app/routes/index.js
@@ -1,18 +1,27 @@
 const router = require('express').Router()
+const { spawn } = require('child_process')
 module.exports = router
 
 router.route('/evaluate')
 .post((req, res, next) => {
-  try{
+  try {
     const model = req.body
     if (!model || !model.input || !model.inputmimetype || !model.transform || !model.outputmimetype) {
       res.status(400).send('Invalid or missing parameters.')
       return
     }
-
-    // TODO: invoke dw native command to get the output
-    res.setHeader('Content-Type', model.inputmimetype)
-    res.send(model.input)
+    // Invoke dw native command to get the output
+    const dw = spawn('dw', [`"${model.transform}"`])
+    let output = ''
+    dw.stdout.on('data', data => {
+      output += data
+    });
+    dw.on('close', code => {
+      res.setHeader('Content-Type', model.outputmimetype)
+      res.send(output)
+    });
+    dw.stdin.write(model.input)
+    dw.stdin.end()
   }
   catch (err) {
     res.status(500).json(err)
